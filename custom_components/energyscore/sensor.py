9e8a4899673d385257e3714c58f1a6613388c73e
Thorjan Knudsvik <t@knudsvik.com>
2022-11-30 22:13:02 +0000

    first sensor update

--- custom_components/energyscore/sensor.py
+++ custom_components/energyscore/sensor.py
@@ -1,7 +1,9 @@
 """Sensor platform for integration_blueprint."""
+import datetime
 from datetime import timedelta
 from typing import Callable, Any
 import logging
+
 import numpy as np
 import voluptuous as vol
 
@@ -18,13 +20,14 @@ from homeassistant.helpers.typing import (
     Optional,
     DiscoveryInfoType,
 )
+from homeassistant.util import dt
 
 from .const import CONF_ENERGY_ENTITY, CONF_PRICE_ENTITY
 
 _LOGGER: logging.Logger = logging.getLogger(__package__)
 
 # Time between updating data TODO: Set to be triggered by a new data point (every whole hour?)
-SCAN_INTERVAL = timedelta(minutes=1)
+SCAN_INTERVAL = timedelta(minutes=10)
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {
@@ -55,8 +58,11 @@ class EnergyScore(SensorEntity):
     _attr_native_unit_of_measurement = "%"
 
     def __init__(self, hass, config):
+        self._current_price = None  # TODO: Not needed?
+        self._current_energy = None  # TODO: Not needed? or maybe define it with :?
         self._name = config[CONF_NAME]
         self._energy_entity = config[CONF_ENERGY_ENTITY]
+        self._last_updated: datetime.datetime | None = None
         self._price_entity = config[CONF_PRICE_ENTITY]
         self._state = None
         self.attr = {
@@ -68,6 +74,9 @@ class EnergyScore(SensorEntity):
             self._attr_unique_id = config[CONF_UNIQUE_ID]
         except:
             pass
+        self._prices = {}
+        self._energy = {}
+        self._energy_total = {}
 
     @property
     def name(self) -> str:
@@ -84,8 +93,32 @@ class EnergyScore(SensorEntity):
 
     async def async_update(self):
         """Updates the sensor"""
+        now = dt.now()  # TODO: Check if this is UTC
+
         try:
-            self._state = self.hass.states.get(self._energy_entity).state
-            # self._state = np.random.random() * 100
+            self._current_price = self.hass.states.get(self._price_entity).state
+            self._current_energy = self.hass.states.get(self._energy_entity).state
+
+            self._prices[now.hour] = self._current_price
+            self._energy_total[now.hour] = self._current_energy
+
+            if now.hour - 1 in self._energy_total:
+                self._energy[now.hour] = (
+                    self._current_energy - self._energy_total[now.hour - 1]
+                )
+
+            _LOGGER.debug(f"EnergyScore price update: {self._prices}")
+            _LOGGER.debug(f"EnergyScore total energy update: {self._energy_total}")
+            _LOGGER.debug(f"EnergyScore energy update: {self._energy}")
+
+            self._last_updated = now.date()
+            self._state = np.random.random() * 100
         except:
             _LOGGER.exception("Could not update the EnergyScore")
+
+
+# TODO: Need to reset the dicts when new day somehow.
+# if self._last_updated is None:
+#    self._prices = {}
+# elif self._last_updated.date() != now.date():
+#    self._prices = {}
