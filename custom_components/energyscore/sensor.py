d3acc61a2ce59ffeaef73ee4cf733bd5a0e4650e
Thorjan Knudsvik <t@knudsvik.com>
2022-12-04 17:57:51 +0000

    refactor, solves #13, solves #12, solves #7

--- custom_components/energyscore/sensor.py
+++ custom_components/energyscore/sensor.py
@@ -61,11 +61,17 @@ class EnergyScore(SensorEntity):
         self._current_price = None  # TODO: Not needed?
         self._current_energy = None  # TODO: Not needed? or maybe define it with :?
         self._energy_total = {}
-        self._name = config[CONF_NAME]
+        self._energy = {}
+        self._energy_array = np.array(None)
         self._energy_entity = config[CONF_ENERGY_ENTITY]
         self._last_updated = None
+        self._name = config[CONF_NAME]
+        self._norm_energy = np.array(None)
+        self._norm_prices = np.array(None)
+        self._price_array = np.array(None)
         self._price_entity = config[CONF_PRICE_ENTITY]
-        self._quality = None
+        self._prices = {}
+        self._quality = 0
         self._state = None
         self._yesterday_energy = None
         self.attr = {
@@ -92,51 +98,88 @@ class EnergyScore(SensorEntity):
     def extra_state_attributes(self):
         return self.attr
 
-    async def async_update(self):
-        """Updates the sensor"""
+    def process_new_data(self):
+        """Processes the update data"""
         now = dt.now()
+        self._quality = len(self._prices) / (int(now.hour) + 1)
+
         if self._last_updated != now.date():
+            self._quality = 0
             self._prices = {}
             self._energy = {}
             if self._last_updated == now.date() - datetime.timedelta(1):
                 self._yesterday_energy = max(self._energy_total.values())
             self._energy_total = {}
 
-        try:
-            self._current_price = round(
-                float(self.hass.states.get(self._price_entity).state), 2
+        self._energy_total[int(now.hour)] = self._current_energy
+        _LOGGER.debug("%s - Total energy update: %s", self._name, self._energy_total)
+
+        if (int(now.hour) - int(1)) in self._energy_total:
+            self._energy[now.hour] = round(
+                (self._current_energy - self._energy_total[int(now.hour) - 1]),
+                2,  # Should maybe check highest key instead (what if hours w/o data)?
             )
-            self._current_energy = round(
-                float(self.hass.states.get(self._energy_entity).state), 2
+        elif self._yesterday_energy is not None:
+            self._energy[now.hour] = round(
+                self._current_energy - self._yesterday_energy, 2
             )
+        else:
+            _LOGGER.debug(
+                "%s - Not enough data to update the EnergyScore yet", self._name
+            )
+            return
 
-            self._prices[int(now.hour)] = self._current_price
-            self._energy_total[int(now.hour)] = self._current_energy
+        self._prices[int(now.hour)] = self._current_price
+        _LOGGER.debug("%s - Energy: %s", self._name, self._energy)
+        _LOGGER.debug("%s - Price: %s", self._name, self._prices)
 
-            if (int(now.hour) - int(1)) in self._energy_total:
-                self._energy[now.hour] = round(
-                    (self._current_energy - self._energy_total[int(now.hour) - 1]), 2
-                )
-            elif self._yesterday_energy != None:
-                self._energy[now.hour] = self._current_energy - self._yesterday_energy
+        self._quality = len(self._prices) / (int(now.hour) + 1)
 
-            _LOGGER.debug(f"Price update: {self._prices}")
-            _LOGGER.debug(f"Energy update: {self._energy}")
-            _LOGGER.debug(f"Total energy update: {self._energy_total}")
-
-            self._last_updated = now.date()
-            self._quality = (len(self._prices) + len(self._energy)) / 2 / int(now.hour)
-
-            self._price_array = np.array(list(self._prices.values()))
-            self._energy_array = np.array(list(self._energy.values()))
+        self._price_array = np.array(list(self._prices.values()))
+        if len(self._prices) > 1:
             self._norm_prices = (self._price_array.max() - self._price_array) / (
                 self._price_array.max() - self._price_array.min()
             )
+        elif len(self._prices) == 1:
+            self._norm_prices = 1
+        _LOGGER.debug("%s - Normalised prices: %s", self._name, self._norm_prices)
+
+        self._energy_array = np.array(list(self._energy.values()))
+        if self._energy_array.sum() == 0:
+            self._norm_energy = list(self._energy.values())
+        else:
             self._norm_energy = self._energy_array / self._energy_array.sum()
+        _LOGGER.debug("%s - Normalised energy: %s", self._name, self._norm_energy)
 
-            _LOGGER.debug(f"Normalised prices: {self.__norm_prices}")
-            _LOGGER.debug(f"Normalised energy: {self._norm_energy}")
+        return round(np.dot(self._norm_prices, self._norm_energy), 1)
 
-            self._state = round(np.dot(self._norm_prices, self._norm_energy), 1)
+    async def async_update(self):
+        """Updates the sensor"""
+        try:
+            self._current_price = self.hass.states.get(self._price_entity).state
+            self._current_energy = self.hass.states.get(self._energy_entity).state
+
+            if (
+                self._current_price == "unavailable"
+                or self._current_energy == "unavailable"
+            ):
+                _LOGGER.exception("%s - Price and/or energy data is unavailable", self._name)
+                return
+            else:
+                self._current_price = round(float(self._current_price), 2)
+                self._current_energy = round(float(self._current_energy), 2)
+
         except:
-            _LOGGER.exception("Could not update the EnergyScore")
+            _LOGGER.exception("%s - Could not fetch price and energy data", self._name)
+        else:
+            try:
+                self._state = self.process_new_data()
+            except:
+                _LOGGER.exception(
+                    "%s - Could not process the updated data and produce the new EnergyScore", self._name
+                )
+            else:
+                self._last_updated = dt.now().date()
+
+
+# TODO: What if not used energy? i.e. same energy in all hours.. 100% score?
