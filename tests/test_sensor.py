c960e1d87c9a7aceadad0f92eb6bbb157e58a756
Thorjan Knudsvik <t@knudsvik.com>
2022-12-13 14:36:10 +0000

    definition

--- tests/test_sensor.py
+++ tests/test_sensor.py
@@ -1,11 +1,11 @@
 from pytest_homeassistant_custom_component.common import MockConfigEntry
 from custom_components.energyscore.const import DOMAIN
 
-from.const import FAKE_CONFIG_DATA
+from .const import FAKE_CONFIG_DATA
 
 
 async def test_energyscore(hass):
-    """Test EnergyScore class."""
+    """Test EnergyScore config."""
     entry = MockConfigEntry(domain=DOMAIN, data=FAKE_CONFIG_DATA)
     entry.add_to_hass(hass)
     assert await hass.config_entries.async_setup(entry.entry_id)
