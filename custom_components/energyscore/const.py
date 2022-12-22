ae44a80a504302178bf9547b47bf8c5b6693bb38
Thorjan Knudsvik <t@knudsvik.com>
2022-12-22 08:18:27 +0000

    cleanup

--- custom_components/energyscore/const.py
+++ custom_components/energyscore/const.py
@@ -1,24 +1,13 @@
 """Constants for EnergyScore"""
 # Base component constants
-NAME = "EnergyScore"
 DOMAIN = "energyscore"
-DOMAIN_DATA = f"{DOMAIN}_data"
-VERSION = "0.0.1"
-ISSUE_URL = "https://github.com/knudsvik/energyscore/issues"
 
 # Icons
 ICON = "mdi:speedometer"
 
-# Platforms
-SENSOR = "sensor"
-PLATFORMS = [SENSOR]
-
 # Configuration and options
 CONF_PRICE_ENTITY = "price_entity"
 CONF_ENERGY_ENTITY = "energy_entity"
-
-# Defaults
-DEFAULT_NAME = DOMAIN
 
 # Other
 ENERGIES = "energy"
