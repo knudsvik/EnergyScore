0a95fb1a65d30c00d50ab1b892d3f49511a679b9
Thorjan Knudsvik <t@knudsvik.com>
2023-01-07 21:59:28 +0000

    Entity Selectors

--- custom_components/energyscore/config_flow.py
+++ custom_components/energyscore/config_flow.py
@@ -3,24 +3,31 @@ ConfigFlow for EnergyScore
 Inspiration from Aaron Godfrey's custom component tutorial, part 3
 """
 
-
-import logging
 from typing import Any
 
 from homeassistant import config_entries
+from homeassistant.components.sensor import DOMAIN as SENSOR_DOMAIN
 from homeassistant.const import CONF_NAME, CONF_UNIQUE_ID
+from homeassistant.helpers import selector
 import homeassistant.helpers.config_validation as cv
 import voluptuous as vol
 
 from .const import CONF_ENERGY_ENTITY, CONF_PRICE_ENTITY, DOMAIN
 
-
 # Use entityselector, see minmax
 CONFIG_SCHEMA = vol.Schema(
     {
-        vol.Required(CONF_NAME): cv.string,
-        vol.Required(CONF_ENERGY_ENTITY): cv.string,
-        vol.Required(CONF_PRICE_ENTITY): cv.string,
+        vol.Required(CONF_NAME): selector.TextSelector(),
+        vol.Required(CONF_ENERGY_ENTITY): selector.EntitySelector(
+            selector.EntitySelectorConfig(
+                domain=[SENSOR_DOMAIN],
+            ),
+        ),
+        vol.Required(CONF_PRICE_ENTITY): selector.EntitySelector(
+            selector.EntitySelectorConfig(
+                domain=[SENSOR_DOMAIN],
+            ),
+        ),
         vol.Required(CONF_UNIQUE_ID): cv.string,
     }
 )
