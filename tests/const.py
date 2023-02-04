3a88c3688dcf7d3d31f72470b44923ad9a6fd47f
Thorjan Knudsvik <t@knudsvik.com>
2023-02-04 21:17:38 +0000

    adds standard energy list

--- tests/const.py
+++ tests/const.py
@@ -12,6 +12,7 @@ ENERGY_DICT = [
 ]
 EMPTY_DICT = [{}, {}]
 
+# Configs
 VALID_CONFIG = {
     "sensor": {
         "platform": "energyscore",
@@ -26,3 +27,47 @@ VALID_UI_CONFIG = {
     "energy_entity": "sensor.energy_ui",
     "price_entity": "sensor.price_ui",
 }
+
+# Common energy and price lists
+ENERGY_LIST = [
+    1,
+    2,
+    3,
+    3.4,
+    3.5,
+    3.7,
+    4.1,
+    5.6,
+    5.7,
+    6.7,
+    6.8,
+    6.9,
+    7.1,
+    7.3,
+    7.7,
+    9.2,
+    9.4,
+    9.8,
+    9.9,
+    10.3,
+    10.8,
+    11.9,
+    14,
+    15.2,
+    16,
+    16.1,
+    16.4,
+    16.9,
+    17,
+    17.4,
+    18,
+    18.2,
+    19.3,
+    20.5,
+    21.6,
+    24.2,
+    26.6,
+    28,
+    32.2,
+    33.3,
+]
